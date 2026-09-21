716820c8adcffa6bc2d2af4a7613705e845d33d8
Eduardo <eduardourbinaaraya@gmail.com>
2026-09-21 08:33:55 -0300

    Redirect uri nueva

--- src/environments/environment.eduardo.production.ts
+++ src/environments/environment.eduardo.production.ts
@@ -12,7 +12,7 @@ export const environment = {
       '4fe52189-087c-4613-8660-500431cc0eb9',
 
     redirectUri:
-      'http://localhost:4200',
+      'https://main.d32i73fsq9ui1t.amplifyapp.com',
 
     apiScope:
       'api://85237aa5-9ece-48d3-8767-9698df0efefa/access_as_user',
