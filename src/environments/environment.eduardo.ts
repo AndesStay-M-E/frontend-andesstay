1d781407796b9b2a1a87779f0d7753ad23aedb44
Eduardo <eduardourbinaaraya@gmail.com>
2026-09-21 03:37:22 -0300

    Agregar configuracion Azure Eduardo

---
+++ src/environments/environment.eduardo.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+
+  apiUrl: 'http://localhost:8080/api',
+
+  azure: {
+    clientId: 'bb7e20d0-d275-4c90-b5ec-99f072c756ce',
+    tenantId: '4fe52189-087c-4613-8660-500431cc0eb9',
+
+    redirectUri: 'http://localhost:4200',
+
+    apiScope:
+      'api://85237aa5-9ece-48d3-8767-9698df0efefa/access_as_user',
+  },
+};
